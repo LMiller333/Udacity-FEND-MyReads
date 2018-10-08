dcb1a966f601b3bf44b2621eca7525d6df06496d
LindsayMiller <lmm566@gmail.com>
2018-10-07 20:01:35 -0400

    Adds query state to search field

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import { Link, Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    showSearchPage: false,
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
   }
 
   render() {
     return (
       <div className="app">
+
         <Route exact path='/search' render={() => (
           <div className="search-books">
             <div className="search-books-bar">
@@ -30,8 +30,7 @@ class BooksApp extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
-
+                <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={ (event) => this.updateQuery(event.target.value)}/>
               </div>
             </div>
             <div className="search-books-results">
@@ -39,6 +38,7 @@ class BooksApp extends React.Component {
             </div>
           </div>
         )}/>
+
         <Route exact path='/' render={() => (
           <div className="list-books">
           <div className="list-books-title">
